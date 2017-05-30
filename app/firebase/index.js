7e30c6c5943e877ace805ccbd65a6aca4c688389
Clint Baker <clintonjamesbaker@gmail.com>
2017-05-29 22:38:06 -0600

    Move firebase credentials to env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,12 +2,12 @@ import firebase from 'firebase';
 
 try {
   var config = {
-    apiKey: "AIzaSyBfm1rye9yESxV1XegFyvWKkyc52jumfwY",
-    authDomain: "cjb-react-todo.firebaseapp.com",
-    databaseURL: "https://cjb-react-todo.firebaseio.com",
-    projectId: "cjb-react-todo",
-    storageBucket: "cjb-react-todo.appspot.com",
-    messagingSenderId: "422250541662"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID
   };
 
   firebase.initializeApp(config);
