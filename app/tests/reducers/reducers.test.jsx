d202863b238499e5ac3b1aee9b9258f837746c16
Clint Baker <clintonjamesbaker@gmail.com>
2017-05-31 22:29:33 -0600

    Additional security features, todos now get removed from state on logout

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -85,6 +85,22 @@ describe('Reducers', () => {
       expect(res.length).toEqual(1);
       expect(res[0]).toEqual(todos[0]);
     });
+
+    it('should wipe todos on logout', () => {
+      var todos = [{
+        id: '111',
+        text: 'anything',
+        completed: false,
+        completedAt: undefined,
+        createdAt: 23445
+      }];
+      var action = {
+        type: 'LOGOUT',
+      };
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res.length).toEqual(0);
+    });
   });
 
   describe('authReducer', () => {
@@ -109,7 +125,7 @@ describe('Reducers', () => {
       };
       var res = reducers.authReducer(df(authData), df(action));
 
-      expect(res).toEqual({});
+      expect(res.length).toNotExist();
     });
   });
 });
